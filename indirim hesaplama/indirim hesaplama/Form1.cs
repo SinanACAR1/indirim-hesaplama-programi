c336125f2d01fd6df936476a5f5a6739f46b8dc1
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:18:14 +0300

    Add files via upload

---
+++ indirim hesaplama/indirim hesaplama/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tutar, indirim, toplam_tutar;
+            tutar = Convert.ToInt32(textBox1.Text);
+            indirim = Convert.ToDouble(textBox2.Text);
+            toplam_tutar = tutar - (tutar * indirim) / 100;
+            label3.Text = toplam_tutar.ToString();
+
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
